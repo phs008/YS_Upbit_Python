1734f100f8e5e176304f96b556dbcc39e2da8632
hspark <hspark@n3n.co.kr>
2019-06-12 17:58:53 +0900

    UpbitAPI 작업중 - static class 변경 - API implementaion 중

--- src/UpbitAPI.py
+++ src/UpbitAPI.py
@@ -1,40 +1,105 @@
 import jwt
 import uuid
 import hashlib
+import requests
 from urllib.parse import urlencode
 
-class UpBit:
-    accessKey = ""
-    secretKey = ""
-    def __init__(self,access_key , secret_key):
-        accessKey = access_key
-        secretKey = secret_key
+
+class UpBitKey(object):
+    accessKey = None
+    secretKey = None
 
 
+class UpBit(UpBitKey):
+    # __instance = None
+
+    # @classmethod
+    # def __getInstance(cls):
+    #     return  cls.__instance
+    #
+    # @classmethod
+    # def instance(cls):
+    #     cls.__instance = cls()
+    #     cls.instance = cls.__instance
+    #     return  cls.__instance
+
+    @staticmethod
+    def instance(access_key, secret_key):
+        UpBitKey.accessKey = access_key
+        UpBitKey.secretKey = secret_key
+
+    @staticmethod
+    def GetMarket():
+        url = "https://api.upbit.com/v1/market/all"
+        return UpBit.HttpRequest(url);
+
+    @staticmethod
+    def GetAccount():
+        url = "https://api.upbit.com/v1/accounts"
+
+    @staticmethod
+    def GetOrderBook(markets):
+        url = "https://api.upbit.com/v1/orderbook"
+        param = ','.join(markets)
+        UpBit.CallApiWithParam(url,param,)
 
 
+    @staticmethod
+    def OrderBook(order):
+        url = "https://api.upbit.com/v1/market/all"
 
 
-    def NoParameter(self):
+    @staticmethod
+    def CallApiNoParam(url, method='GET'):
+        try:
+            token = UpBit.NoParameterRequest()
+            res = requests.request(method, url)
+            res.raise_for_status()
+            return res.json()
+        except requests.exceptions.RequestException as e:
+            print(e)
+            pass
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            pass
+
+    @staticmethod
+    def CallApiWithParam(url, body, method='GET'):
+        try:
+            token = UpBit.WithParameterRequest(body)
+            res = requests.request(method, url)
+            res.raise_for_status()
+            return res.json()
+        except requests.exceptions.RequestException as e:
+            print(e)
+            pass
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            pass
+
+    @classmethod
+    def NoParameterRequest(cls):
         payload = {
-            'access_key' : self.accessKey,
-            'nonce' : str(uuid.uuid4()),
+            'access_key': UpBit.accessKey,
+            'nonce': str(uuid.uuid4()),
         }
-        jwt_token = jwt.encode(payload,self.secretKey).decode('utf8')
+        jwt_token = jwt.encode(payload, UpBit.secretKey).decode('utf8')
         authorization_token = 'Bearer {}'.format(jwt_token)
-        return  authorization_token
 
-    def WithParameter(self,queryDic):
+        return authorization_token
+
+    @staticmethod
+    def WithParameterRequest(queryDic):
         hash = hashlib.sha512()
         hash.update(urlencode(queryDic).encode())
         query_hash = hash.hexdigest()
 
         payload = {
-            'access_key': self.accessKey,
+            'access_key': UpBit.accessKey,
             'nonce': str(uuid.uuid4()),
-            'query_hash' : query_hash,
-            'query_hash_alg' : 'SHA512',
+            'query_hash': query_hash,
+            'query_hash_alg': 'SHA512',
         }
-        jwt_token = jwt.encode(payload, self.secretKey).decode('utf8')
+        jwt_token = jwt.encode(payload, UpBit.secretKey).decode('utf8')
         authorization_token = 'Bearer {}'.format(jwt_token)
         return authorization_token
--- src/YS_Upbit.py
+++ src/YS_Upbit.py
@@ -3,8 +3,7 @@ import sys
 import time
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
-
-from watchdog.events import PatternMatchingEventHandler
+from src.UpbitAPI import UpBit
 
 
 class FileHandler(PatternMatchingEventHandler):
@@ -19,6 +18,7 @@ class FileHandler(PatternMatchingEventHandler):
                     line = f.readline()
                     if len(line) == 0:
                         print('beforeLine : [', beforeLine, '] lastLien : [', lastLine, ']')
+
                         break;
                     beforeLine = lastLine
                     lastLine = line
@@ -32,13 +32,14 @@ class FileHandler(PatternMatchingEventHandler):
         self.FileParsing(event)
 
 
-
 if __name__ == "__main__":
     pattenrs = ["*.txt", "*.text"]
     # logging.basicConfig(level=logging.INFO,
     #                     format='%(asctime)s - %(message)s',
     #                     datefmt='%Y-%m-%d %H:%M:%S')
     path = sys.argv[1] if len(sys.argv) > 1 else '.'
+    UpBit.instance('bCx3A5fO8kWfoM5WrNTtdbx9l9xRvaIGnCB3uSjy','LXZHyqdxIgexfYqNokGt2xvNR1sKNixirMDauhca')
+    result = UpBit.GetMarket()
     event_handler = FileHandler(pattenrs)
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
